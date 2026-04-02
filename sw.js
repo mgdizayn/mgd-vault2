c2fe29825ff35ffcfe6b82d23a16fcb4db77bcdd
mgdizayn <mgdizayn@hotmail.com>
2026-04-02 21:21:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,38 @@
-var C='mgd-v2';var A=['/','/index.html','/manifest.json','/css/vars.css','/css/base.css','/css/lock.css','/css/dial.css','/css/screens.css','/js/noise.js','/js/crypto.js','/js/router.js','/js/dial.js','/js/markdown.js','/js/editor.js','/js/app.js'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(A);}));self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==C;}).map(function(x){return caches.delete(x);}));}));self.clients.claim();});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
+var C='mgd-v3';
+var BASE='/mgd-vault2';
+var A=[
+  BASE+'/',
+  BASE+'/index.html',
+  BASE+'/manifest.json',
+  BASE+'/css/vars.css',
+  BASE+'/css/base.css',
+  BASE+'/css/lock.css',
+  BASE+'/css/dial.css',
+  BASE+'/css/screens.css',
+  BASE+'/js/noise.js',
+  BASE+'/js/crypto.js',
+  BASE+'/js/router.js',
+  BASE+'/js/dial.js',
+  BASE+'/js/markdown.js',
+  BASE+'/js/editor.js',
+  BASE+'/js/app.js',
+  BASE+'/icons/icon-192.png',
+  BASE+'/icons/icon-512.png',
+];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(C).then(function(c){return c.addAll(A);}));
+  self.skipWaiting();
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(k){
+    return Promise.all(k.filter(function(x){return x!==C;}).map(function(x){return caches.delete(x);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(caches.match(e.request).then(function(r){
+    return r||fetch(e.request).catch(function(){
+      return caches.match(BASE+'/index.html');
+    });
+  }));
+});
